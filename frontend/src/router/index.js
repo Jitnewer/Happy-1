c2fe99aef61c3814dd59eae13713c715f729ba1d
jitne <jit.newer@hva.nl>
2023-10-05 13:10:08 +0200

    #17 -> Created admin event management page.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+import AdminEventView from '@/components/AdminEventView.vue'
+import EventDetailsView from '@/components/EventDetailsView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes: [
+    { path: '/', redirect: '/adminEventView' },
+    {
+      path: '/adminEventView',
+      component: AdminEventView,
+      children: [
+        { path: ':id', component: EventDetailsView }
+      ]
+    }
+  ]
 })
 
 export default router
