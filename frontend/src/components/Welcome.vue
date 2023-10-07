1827a6b60e59caefd65bf23ab5e9e19aa906c287
nicky <nicky.bosveld@hva.nl>
2023-10-07 17:21:56 +0200

    Imported the path to the router

---
+++ frontend/src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p>Hello
+  </p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
+import Welcome from '@/components/Welcome.vue'
 const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'welcome',
+    component: Welcome
+  }
 ]
 
 const router = createRouter({
