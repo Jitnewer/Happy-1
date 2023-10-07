13ccff630a52cc51f6f020ab01588a41d2139e54
nicky <nicky.bosveld@hva.nl>
2023-10-07 21:00:17 +0200

    made a start on the carousel

---
+++ frontend/src/components/Carousel.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="carousel">
+    <slot :currentSlide="currentSlide"/>
+
+    <div class="navigate">
+      <div class="toggle-page left">
+        <i @click="prevSlide" class="fa-solid fa-arrow-left" style="color: #000000;"></i>
+      </div>
+      <div class="toggle-page right">
+        <i @click="nextSlide" class="fa-solid fa-arrow-left fa-rotate-180" style="color: #000000;"></i>
+      </div>
+      <div class="pagination">
+      <span v-for="(slide, index) in getSlideCount"
+            :key="index"
+            :class="{active : index + 1 === currentSlide}">
+
+      </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  name: 'Carousel',
+  setup () {
+    const currentSlide = ref(1)
+    const getSlideCount = ref(null)
+    const nextSlide = () => {
+      if (currentSlide.value === getSlideCount.value) {
+        currentSlide.value = 1
+        return
+      }
+      currentSlide.value += 1
+    }
+    const prevSlide = () => {
+      if (currentSlide.value === 1) {
+        currentSlide.value = 1
+        return
+      }
+      currentSlide.value -= 1
+    }
+    onMounted(() => {
+      getSlideCount.value = document.querySelectorAll('.slide').length
+    })
+
+    return { currentSlide, nextSlide, prevSlide, getSlideCount }
+  }
+}
+</script>
+
+<style scoped>
+.navigate {
+  padding: 0 16px;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.toggle-page {
+  display: flex;
+  flex: 1;
+}
+
+.right {
+  justify-content: flex-end;
+}
+
+i {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  background-color: blue;
+}
+.pagination {
+  position: absolute;
+  bottom: 24px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+}
+ span{
+   cursor: pointer;
+   width: 20px;
+   height: 20px;
+   border-radius: 50%;
+   background-color: black;
+   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+ }
+</style>
--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,18 +1,46 @@
 <template>
 <div class="container">
-
+  <Carousel class="carousel" v-slot="{ currentSlide }">
+    <Slide v-for="(slide, index) in carouselSlides" :key="index">
+      <div v-show="currentSlide === index + 1" class="slide">
+        <img :src="require(`@/assets/img/carousel/${slide}.jpg`)" alt="">
+      </div>
+    </Slide>
+  </Carousel>
 </div>
 </template>
 
 <script>
+import Carousel from '../components/Carousel.vue'
+import Slide from '../components/Slide.vue'
 export default {
   name: 'Welcome.vue',
-  mounted () {
-    console.log('component wordt ingeladen')
+  components: { Carousel, Slide },
+  data () {
+    return {
+      carouselSlides: ['bg-1', 'bg-2', 'bg-3']
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.carousel {
+  position: relative;
+  max-height: 600px;
+  max-width: 600px;
+}
+.slide-info {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  max-height: 100%;
+  height: 100%;
+}
+ /*img {*/
+ /*  min-width: 100%;*/
+ /*  height: 100%;*/
+ /*  object-fit: cover;*/
+ /*}*/
 </style>
