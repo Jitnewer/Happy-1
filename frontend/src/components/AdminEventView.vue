2d9c4908c7b3909fd0fc6950efd243bb28c07755
Rick Veerman <rick.veerman2@hva.nl>
2023-10-09 18:15:41 +0200

    main -> integrated #17

--- frontend/src/components/AdminEventView.vue
+++ frontend/src/components/AdminEventView.vue
@@ -177,4 +177,7 @@ export default {
 </template>
 
 <style scoped>
+h1, h2, h3, h4, h5, h6, p {
+  color: black
+}
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
-const routes = [
-]
 import AdminEventView from '@/components/AdminEventView.vue'
 import EventDetailsView from '@/components/EventDetailsView.vue'
 
+const routes = [
+  {
+    path: '/admin/event',
+    component: AdminEventView,
+    children: [
+      { path: ':id', component: EventDetailsView }
+    ]
+  }
+]
+
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/adminEventView',
-      component: AdminEventView,
-      children: [
-        { path: ':id', component: EventDetailsView }
-      ]
-    }
-  ]
+  routes
 })
 
 export default router
