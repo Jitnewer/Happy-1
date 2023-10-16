7d80dfd5156fe637d85e908b90595d2f0462e883
Almaz Fkyerat <fkyeraa1@id.hva.nl>
2023-10-16 23:28:01 +0200

    Login - form #16

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <nav-bar></nav-bar>
+  <log-in></log-in>
 </template>
 
 <script>
 
 import NavBar from '@/components/NavBar.vue'
+import LogIn from '@/components/Log-in-.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    LogIn
   }
 }
 
---
+++ frontend/src/components/Log-in-.vue
@@ -0,0 +1,131 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="sign-up-body">
+    <div class="container-sign-up">
+      <div class="header-sign-up">Login:</div>
+      <div class="form-sign-up-1">
+        <form action="#">
+          <div class="sign-up-page">
+            <div class="title-sign-up-form">Login with your email:</div>
+            <div class="field-sign-up">
+              <div class="label-sign-up">Email:</div>
+              <input type="email" class="sign-up-input" required>
+            </div>
+
+            <div class="field-sign-up">
+              <div class="label-sign-up">Password:</div>
+              <input type="password" class="sign-up-input" required>
+            </div>
+
+            <div class="field-btn">
+              <button class="submit">Log in</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+/*Sign up form */
+.sign-up-body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.container-sign-up {
+  width: 330px;
+  background: #fff;
+  border-radius: 5px;
+  text-align: center;
+  padding: 50px 35px 10px 35px;
+  box-shadow: 0px 3px 40px #000;
+  margin-top: 60px;
+}
+
+.header-sign-up {
+  font-size: 35px;
+  font-weight: 600;
+  margin: 0 0 30px 0;
+}
+
+.container-sign-up .form-sign-up-1 {
+  width: 100%;
+  overflow: hidden;
+}
+
+.form-sign-up-1 form {
+  display: flex;
+  width: 400%;
+}
+
+.form-sign-up-1 form .sign-up-page {
+  width: 25%;
+}
+
+.title-sign-up-form {
+  text-align: left;
+  font-size: 25px;
+  font-weight: 500;
+}
+
+.field-sign-up {
+  height: 45px;
+  width: 330px;
+  margin: 45px 0;
+  display: flex;
+  position: relative;
+}
+
+.label-sign-up {
+  position: absolute;
+  top: -30px;
+  font-weight: 500;
+}
+
+.sign-up-input {
+  height: 100%;
+  width: 100%;
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  font-size: 18px;
+  padding-left: 15px;
+}
+
+.field-btn button {
+  width: 100%;
+  height: 50px;
+  margin-bottom: 10px;
+  margin-top: -20px;
+  border: none;
+  background: #0992A4;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 500;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: 0.3s ease;
+}
+
+.field-btn button:hover {
+  background: #000;
+}
+
+.span-sign-up-form-check span{
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 25px;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+}
+</style>
