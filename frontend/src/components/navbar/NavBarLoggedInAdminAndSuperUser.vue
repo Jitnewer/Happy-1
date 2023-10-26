867d790d1ea6afaba16aa6946360c95349966a79
Rick Veerman <rick.veerman2@hva.nl>
2023-10-26 15:38:29 +0200

    fixed transitions

--- frontend/src/components/navbar/NavBarLoggedInAdminAndSuperUser.vue
+++ frontend/src/components/navbar/NavBarLoggedInAdminAndSuperUser.vue
@@ -28,8 +28,8 @@
         </div>
       </div>
   </nav>
+  <div class="dashboard-container">
   <transition name="dashboard-big">
-    <div class="dashboard-container">
   <div class="dashboard" v-show="showDashboard && !smallVersionDasboard">
     <div class="profile-dashboard" >
       <canvas ref="profileCanvasDashboard" class="profile-photo-dashboard" width="45" height="45"></canvas>
@@ -50,11 +50,9 @@
       </router-link>
     </div>
   </div>
-      <router-view></router-view>
-    </div>
   </transition>
   <transition name="dashboard-small">
-  <div class="dashboard" v-show="smallVersionDasboard">
+  <div class="dashboard small" v-show="smallVersionDasboard">
     <div class="dashboard-links-small">
       <router-link  to="/admin/users">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
@@ -68,6 +66,8 @@
     </div>
   </div>
   </transition>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
