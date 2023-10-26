e7ecf45394ce4131559073b31b5c3daa7863dd7a
Rick Veerman <rick.veerman2@hva.nl>
2023-10-26 13:14:13 +0200

    created a route path

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import AdminEventView from '@/components/AdminEventView.vue'
 import EventDetailsView from '@/components/EventDetailsView.vue'
+import AboutUs from '@/components/AboutUs.vue'
 
 const routes = [
+  {
+    path: '/about-us',
+    component: AboutUs
+  },
   {
     path: '/admin/event',
     component: AdminEventView,
