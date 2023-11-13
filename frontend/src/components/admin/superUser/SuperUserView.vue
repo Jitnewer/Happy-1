5f854880cf99fad01a6cc29e85ee3358528713a1
jitne <jit.newer@hva.nl>
2023-11-13 21:29:26 +0100

    moved the admin components to one place

--- frontend/src/components/admin/superUser/SuperUserView.vue
+++ frontend/src/components/admin/superUser/SuperUserView.vue
@@ -20,7 +20,7 @@ export default {
       this.selectedNews = new News()
       this.selectedNews.id = (this.idUse + 1)
       this.idUse++
-      this.selectedNews.image = require('../../assets/images/imagePlaceholder.jpg')
+      this.selectedNews.image = require('../../../assets/images/imagePlaceholder.jpg')
       this.createNews = true
       this.isSelected = !this.isSelected
       this.$router.push(this.$route.matched[0].path + '/' + this.selectedNews.id)
