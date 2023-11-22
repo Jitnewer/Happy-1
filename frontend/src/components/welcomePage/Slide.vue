711fcd1e0ca45c71f9ebcbc5fe295d9ad6b21b87
nicky <nicky.bosveld@hva.nl>
2023-11-22 16:59:12 +0100

    made carousel text switch with each slide

--- frontend/src/components/welcomePage/Slide.vue
+++ frontend/src/components/welcomePage/Slide.vue
@@ -1,17 +1,24 @@
 <template>
-<div class="Slide">
-  <transition name="slide">
-    <slot/>
-  </transition>
-</div>
+  <div class="Slide">
+    <transition name="slide">
+      <div v-show="isCurrent">
+        <slot/>
+        <p v-if="slideText">{{ slideText }}</p>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Slide'
+  name: 'Slide',
+  props: {
+    slideText: String,
+    isCurrent: Boolean
+  }
 }
 </script>
 
 <style scoped>
-
+/* Voeg hier je stijlen toe indien nodig */
 </style>
